25c52b08461ac5ecfd2c4fa94fe94265336c1ffa
areeba <areebs101@gmail.com>
2025-09-08 12:38:46 -0400

    update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { createServer } from "./server";
 
+
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
